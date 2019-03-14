e8b430990a9e5444b596fef4b2ace951610b743b
Jose M. Rodriguez <jmrodriguez98842@gmail.com>
2019-03-14 11:27:29 -0500

    original 5 numbers

---
+++ inClass8.py
@@ -0,0 +1,12 @@
+lst = []
+num = 5
+
+for n in range(num) :
+    numbers = int(input('Enter number: '))
+    lst.append(numbers)
+
+
+print('Largest number is:', max(lst), 'Minimum number is:', min(lst))
+
+
+
