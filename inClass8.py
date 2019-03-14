8fa3cc525a3283a5f26bd9df8c27416e11b92a10
Jose M. Rodriguez <jmrodriguez98842@gmail.com>
2019-03-14 12:04:07 -0500

    person A changes

--- inClass8.py
+++ inClass8.py
@@ -2,11 +2,15 @@ lst = []
 num = 5
 
 for n in range(num) :
-    numbers = int(input('Enter number: '))
+    numbers = int(input('Enter number between 0 and 10,000: '))
+    while (numbers < 0 or numbers > 10000):
+        print('Numbers must be between 0 and 10,0000')
+        numbers = int(input('Enter number: '))
     lst.append(numbers)
 
 
-print('Largest number is:', max(lst), 'Minimum number is:', min(lst))
+
+print('Largest number is:', max(lst), 'Smallest number is:', min(lst))
 
 
 
